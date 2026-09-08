384d681308a7eb97b79b6132d0200795cd77d2f7
Rodney Beede <rbeede@users.noreply.github.com>
2026-09-07 21:41:14 -0500

    Simplify logging in Main.java: remove programmatic setupLogging and rely on log4j2.xml on the classpath.

--- src/main/java/com/rodneybeede/software/hashdirectorytree/Main.java
+++ src/main/java/com/rodneybeede/software/hashdirectorytree/Main.java
@@ -1,7 +1,5 @@
 package com.rodneybeede.software.hashdirectorytree;
 
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,156 +11,95 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-import java.util.Date;
-
 import org.apache.commons.codec.digest.DigestUtils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.Layout;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.appender.FileAppender;
-import org.apache.logging.log4j.core.layout.PatternLayout;
 
 
 /**
  * Output is https://tools.ietf.org/html/rfc4180
- * 
- * @author rbeede
  *
+ * Simplified logging: configuration is provided via src/main/resources/log4j2.xml on the classpath.
+ *
+ * @author rbeede
  */
 public class Main {
-	private static final Logger log = LogManager.getLogger(Main.class);
-	
-	public static void main(final String[] args) throws IOException, InterruptedException {
-		if(null == args || args.length != 2) {
-			System.err.println("Incorrect number of arguments");
-			System.out.println("Usage:  java -jar " + Main.class.getProtectionDomain().getCodeSource().getLocation().getFile() + " <source directory> <csv output file>");
-			System.exit(255);
-			return;
-		}
-		
-		
-		setupLogging();
-				
-		
-		// Parse config options as Canonical paths
-		final Path sourceDirectory = Paths.get(args[0]).toRealPath();
-		final Path csvOutputFilePath = Paths.get(args[1]).toAbsolutePath();  // RealPath doesn't exist yet
-		
-		log.info("Source directory (real canonical) is {}", sourceDirectory);
-		log.info("CSV report output file path (real canonical) is {}", csvOutputFilePath);
+    private static final Logger log = LogManager.getLogger(Main.class);
 
-		
-		final FileWriter fwriter = new FileWriter(csvOutputFilePath.toFile());
-		fwriter.write("Size-bytes,Hash,File");  // HEADER
-		// Don't write the \r\n yet as we do that for each row we add
-		// Saves us from having to leave-off a trailing \r\n at the very end
-		
-		Files.walkFileTree(sourceDirectory, new FileVisitor<Path>() {
+    public static void main(final String[] args) throws IOException, InterruptedException {
+        if (null == args || args.length != 2) {
+            System.err.println("Incorrect number of arguments");
+            System.out.println("Usage:  java -jar " + Main.class.getProtectionDomain().getCodeSource().getLocation().getFile() + " <source directory> <csv output file>");
+            System.exit(255);
+            return;
+        }
 
-			@Override
-			public FileVisitResult postVisitDirectory(Path arg0, IOException arg1) throws IOException {
-				return FileVisitResult.CONTINUE;
-			}
+        // Logging is configured via src/main/resources/log4j2.xml on the classpath
 
-			@Override
-			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-				return FileVisitResult.CONTINUE;
-			}
+        // Parse config options as Canonical paths
+        final Path sourceDirectory = Paths.get(args[0]).toRealPath();
+        final Path csvOutputFilePath = Paths.get(args[1]).toAbsolutePath();  // RealPath doesn't exist yet
 
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				log.info("Hashing {}", file);
-				
-				try {
-					final FileInputStream fis = new FileInputStream(file.toFile());
-					final String hash = org.apache.commons.codec.digest.DigestUtils.md5Hex(fis);
-					fis.close();
-					
-					fwriter.write("\r\n");
-					
-					fwriter.write(Long.toString(Files.size(file)));
-					fwriter.write(',');
-					fwriter.write(hash);
-					fwriter.write(',');
-					fwriter.write('"');  // double quote the third field
-					fwriter.write(file.toString().replace("\"", "\"\""));  // All internal " with double  ""
-					fwriter.write('"');  // close of third field
-					// Don't write \r\n here, only for next row if any (no trailing \r\n at end of file)
-					fwriter.flush();
-				} catch(final IOException excep) {
-					log.error(excep,excep);
-				}
+        log.info("Source directory (real canonical) is {}", sourceDirectory);
+        log.info("CSV report output file path (real canonical) is {}", csvOutputFilePath);
 
-				
-				return FileVisitResult.CONTINUE;
-			}
+        final FileWriter fwriter = new FileWriter(csvOutputFilePath.toFile());
+        fwriter.write("Size-bytes,Hash,File");  // HEADER
+        // Don't write the \r\n yet as we do that for each row we add
+        // Saves us from having to leave-off a trailing \r\n at the very end
 
-			@Override
-			public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-				log.error("Failed to access:  {}", file, exc);
-				
-				return FileVisitResult.CONTINUE;
-			}
-			
-		});
-		
-		
-		fwriter.close();  // flushes too
-		
-		// Exit with appropriate status
-		log.info("Program has completed");
-		
+        Files.walkFileTree(sourceDirectory, new FileVisitor<Path>() {
 
-		LogManager.shutdown();;  //Forces log to flush
-		
-		System.exit(0);  // All good
-	}
-	
-	
-	private static void setupLogging() {
-		Layout<?> layout = PatternLayout.newBuilder()
-				.withPattern("%d{yyyy-MM-dd HH:mm:ss,SSS Z}\t%-5p\tThread=%t\t%c\t%m%n")
-				.build();
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
 
-		ConsoleAppender consoleAppender = ConsoleAppender.newBuilder()
-				.setName("Console")
-				.setLayout(layout)
-				.build();
-		consoleAppender.start();
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
 
-		final File logFile = new File(System.getProperty("user.dir"), "HDT_" + getFormattedDatestamp(null) + ".log");
-		FileAppender fileAppender = FileAppender.newBuilder()
-				.withFileName(logFile.getAbsolutePath())
-				.withName("File")
-				.withLayout(layout)
-				.build();
-		fileAppender.start();
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                log.info("Hashing {}", file);
 
-		org.apache.logging.log4j.core.Logger rootLogger =
-				(org.apache.logging.log4j.core.Logger) LogManager.getRootLogger();
+                try (FileInputStream fis = new FileInputStream(file.toFile())) {
+                    final String hash = DigestUtils.md5Hex(fis);
 
-		rootLogger.addAppender(consoleAppender);
-		rootLogger.addAppender(fileAppender);
-		rootLogger.setLevel(Level.INFO);
+                    fwriter.write("\r\n");
 
+                    fwriter.write(Long.toString(Files.size(file)));
+                    fwriter.write(',');
+                    fwriter.write(hash);
+                    fwriter.write(',');
+                    fwriter.write('"');  // double quote the third field
+                    fwriter.write(file.toString().replace("\"", "\"\""));  // All internal " with double  ""
+                    fwriter.write('"');  // close of third field
+                    // Don't write \r\n here, only for next row if any (no trailing \r\n at end of file)
+                    fwriter.flush();
+                } catch (final IOException excep) {
+                    log.error("Error hashing file {}", file, excep);
+                }
 
-		System.out.println("Logging to " + logFile.getAbsolutePath());
-	}
-	
-	
-	private static String getFormattedDatestamp(final Date date) {
-		final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss_Z");
-		
-		if(null == date) {
-			return dateFormat.format(new Date());
-		} else {
-			return dateFormat.format(date);
-		}
-	}
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                log.error("Failed to access:  {}", file, exc);
+
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+
+        fwriter.close();  // flushes too
+
+        // Exit with appropriate status
+        log.info("Program has completed");
+
+        System.exit(0);  // All good
+    }
 }
